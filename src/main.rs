5a8825b6c7bed06efceb40ef47bd2bb34e334c15
myFavShrimp <myfavshrimp@tuta.io>
2023-07-12 10:52:17 +0200

    Added -h and --help args

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,24 @@ mod config;
 mod home_dir;
 mod linking;
 
+static CRATE_VERSION: &'static str = env!("CARGO_PKG_VERSION");
+static CRATE_NAME: &'static str = env!("CARGO_PKG_NAME");
+static CRATE_DESCRIPTION: &'static str = env!("CARGO_PKG_DESCRIPTION");
+static HELP_TEXT: &'static str = "Usage:\n  Run 'lnkr' in a directory containing a 'lnkr.yaml'";
+
+fn help_text() -> String {
+    format!(
+        "{} {} - {}\n\n{}",
+        CRATE_NAME, CRATE_VERSION, CRATE_DESCRIPTION, HELP_TEXT
+    )
+}
+
 fn main() -> eyre::Result<()> {
+    if let Some(_) = std::env::args().position(|arg| arg == "--help" || arg == "-h") {
+        println!("{}", help_text());
+        return Ok(());
+    }
+
     let mut config_path = std::env::current_dir()?;
     config_path.push(config::CONFIG_FILE_NAME);
 
